15f1f3870b7755f56b15d6a957c73c677168e6b2
Michael Dreesen <michaeldreesen90@gmail.com>
2022-01-01 12:27:14 -0500

    taking out dropdown for now

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -36,13 +36,13 @@ const Navigation = () => {
                         {
                             NavLink.map(nav => <Nav.Link href={nav?.link}>{nav?.name}</Nav.Link>)
                         }
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                        {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
