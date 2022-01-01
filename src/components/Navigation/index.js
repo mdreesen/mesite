7211286e1ad487ad7789fea4104273a55bfa715f
Michael Dreesen <michaeldreesen90@gmail.com>
2022-01-01 12:29:14 -0500

    commented out import

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './navigation.css';
 
