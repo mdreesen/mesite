b12466d0969e15798f0c0402d1e318c0daa445c6
Michael Dreesen <mdreesen@Mac.lan>
2026-02-20 16:52:21 -0700

    feature(Updating projects)

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -28,31 +28,6 @@ const stratacacheFeatures = {
   details: "My position with this digital signage company is to make applications for fast food companies. I am the Lead Developer for Tim Horton's US and Subway US."
 };
 
-const farmdirectoryFeatures = {
-  title: "The Farm Directory",
-  description: "Connecting farms to communities.",
-  logoPath: { path: "/assets/company_logos/thefarmdirectory.webp", alt: 'The Farm Directory logo' },
-  frontend: ['Next.js'],
-  design: ['Tailwind'],
-  language: ['Typescript'],
-  database: ['MongoDB'],
-  deployed: ['Vercel'],
-  extraFeatures: ['Radar API', "React Google Maps", "Nodemailer", "Mailtrap", "React Share", "Next Auth", "Mongoose", "Uploadthing", "Nanoid", "Google Analytics", "Vercel Analytics"],
-  details: "A Project to help local farmers be found and sell their product to users in their area."
-};
-
-const farmdirectoryAdminFeatures = {
-  title: "Farm Directory Admin",
-  description: "Admin dashboard to control The Farm Directory.",
-  logoPath: { path: "/assets/company_logos/thefarmdirectory.webp", alt: 'The Farm Directory Admin logo' },
-  frontend: ['Next.js'],
-  design: ['Tailwind'],
-  language: ['Typescript'],
-  database: ['MongoDB'],
-  deployed: ['Vercel'],
-  details: "A Project to dedicate admins to have control over its users and visualize data for The Farm Directory."
-};
-
 const radminFeatures = {
   title: "Radmin",
   description: "Admin application to help businesses take control of their business.",
@@ -61,7 +36,7 @@ const radminFeatures = {
   design: ['Tailwind'],
   language: ['Typescript'],
   deployed: ['Vercel'],
-  details: "A Project to dedicated to businesses to help them take control of their business. They can view customers, invoices, team members, and see their revenue."
+  details: "A Project dedicated to businesses to help them take control of their revenue, customers, and team. They can view customers, invoices, team members, and see their revenue."
 };
 
 const ascendFeatures = {
@@ -69,13 +44,13 @@ const ascendFeatures = {
   description: "Set goals. Achieve greatness.",
   logoPath: { path: "/assets/company_logos/ascend.webp", alt: 'Ascend logo' },
   googlePlayBadge: { path: "/assets/badges/google_play_badge.webp", alt: 'Ascend logo' },
-  frontend: ['Next.js'],
+  frontend: ['Nuxt.js'],
   design: ['Tailwind'],
   language: ['Typescript'],
   database: ['MongoDB'],
   deployed: ['Vercel'],
-  extraFeatures: ["Next Auth", "Mongoose", "PWA", "Google Play App"],
-  details: "A Project to dedicated to have a better you! Set your habits, affirmations. Track your weight, water intake for the day, set a fasting time, and books that you are reading or listening to.",
+  extraFeatures: ["Nuxt Auth", "Mongoose", "PWA", "Google Play App"],
+  details: "A Project dedicated to have a better you! Set your habits, affirmations. Track your weight, water intake for the day, set a fasting time, and books that you are reading or listening to.",
 };
 
 const westernRockiesConstruction = {
@@ -93,8 +68,8 @@ export default function Page() {
 
   // Linwood
   const projectOne = (
-    <div className="relative lg:row-span-2">
-      <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+    <div className="relative w-[26rem]">
+      <div className="absolute inset-px rounded-lg bg-white"></div>
       <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
         <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
           <p className="mt-2 text-xl font-medium tracking-tight text-gray-950 max-lg:text-center">
@@ -139,13 +114,13 @@ export default function Page() {
           </div>
         </div>
       </div>
-      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
+      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
     </div>
   );
 
   // STRATACACHE
   const projectTwo = (
-    <div className="relative max-lg:row-start-1">
+    <div className="relative w-[26rem]">
       <div className="absolute inset-px rounded-lg bg-gray-900 max-lg:rounded-t-[2rem]"></div>
       <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] py-4">
         <div className="px-8 pt-4 sm:px-10 sm:pt-6">
@@ -181,109 +156,11 @@ export default function Page() {
     </div>
   );
 
-  // The Farm Directory Admin
-  const projectThree = (
-    <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-      <div className="absolute inset-px rounded-lg bg-white"></div>
-      <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
-        <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-          <p className="mt-2 text-xl font-medium tracking-tight text-gray-950 max-lg:text-center">{farmdirectoryAdminFeatures.title}</p>
-          <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-            {farmdirectoryAdminFeatures.description}
-          </p>
-
-
-          <div className="max-w-lg text-sm/6 text-gray-600 max-lg:text-center py-4">
-            <h3 className='text-gray-600 text-lg'>Features</h3>
-
-            {farmdirectoryAdminFeatures.frontend.map((item: string) => <p key={item}>{item}</p>)}
-            {farmdirectoryAdminFeatures.design.map((item: string) => <p key={item}>{item}</p>)}
-            {farmdirectoryAdminFeatures.language.map((item: string) => <p key={item}>{item}</p>)}
-            {farmdirectoryAdminFeatures.database.map((item: string) => <p key={item}>{item}</p>)}
-            {farmdirectoryAdminFeatures.deployed.map((item: string) => <p key={item}>{item}</p>)}
-
-            <h3 className='text-gray-600 text-lg mt-2'>About</h3>
-            {farmdirectoryAdminFeatures.details}
-          </div>
-
-          <div className="mt-10 flex items-center gap-x-6">
-            <a href="https://admin-farm-directory.vercel.app/" target="_blank" className="text-sm/6 font-semibold text-gray-900">
-              Website <span aria-hidden="true">→</span>
-            </a>
-          </div>
-          <div className="mt-10 flex items-center gap-x-6">
-            <a href="https://github.com/mdreesen/admin-farm-directory" target="_blank" className="text-sm/6 font-semibold text-gray-900">
-              Github <span aria-hidden="true">→</span>
-            </a>
-          </div>
-        </div>
-        <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
-          {/* Can place image here  if need be */}
-        </div>
-      </div>
-      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
-    </div>
-  );
-
-  // The Farm Directory
-  const projectFour = (
-    <div className="relative lg:row-span-2">
-      <div className="absolute inset-px rounded-lg bg-white lg:rounded-r-[2rem]"></div>
-      <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-r-[calc(2rem+1px)]">
-        <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
-          <p className="mt-2 text-xl font-medium tracking-tight text-gray-950 max-lg:text-center">
-            {farmdirectoryFeatures.title}
-          </p>
-          <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-            {farmdirectoryFeatures.description}
-          </p>
-
-          <div className="max-w-lg text-sm/6 text-gray-600 max-lg:text-center py-4">
-            <h3 className='text-gray-600 text-lg'>Features</h3>
-
-            {farmdirectoryFeatures.frontend.map((item: string) => <p key={item}>{item}</p>)}
-            {farmdirectoryFeatures.design.map((item: string) => <p key={item}>{item}</p>)}
-            {farmdirectoryFeatures.language.map((item: string) => <p key={item}>{item}</p>)}
-            {farmdirectoryFeatures.database.map((item: string) => <p key={item}>{item}</p>)}
-            {farmdirectoryFeatures.deployed.map((item: string) => <p key={item}>{item}</p>)}
-            {farmdirectoryFeatures.extraFeatures.map((item: string) => <p key={item}>{item}</p>)}
-
-            <h3 className='text-gray-600 text-lg mt-2'>About</h3>
-            {farmdirectoryFeatures.details}
-          </div>
-
-          <div className="mt-10 flex items-center gap-x-6">
-            <a href="https://thefarmdirectory.com/" target="_blank" className="text-sm/6 font-semibold text-gray-900">
-              Website <span aria-hidden="true">→</span>
-            </a>
-          </div>
-          <div className="mt-10 flex items-center gap-x-6">
-            <a href="https://github.com/mdreesen/farm-directory" target="_blank" className="text-sm/6 font-semibold text-gray-900">
-              Github <span aria-hidden="true">→</span>
-            </a>
-          </div>
-        </div>
-        <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
-          <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-white shadow-2xl">
-            <Image
-              alt={farmdirectoryFeatures.logoPath.alt}
-              width={200}
-              height={200}
-              src={farmdirectoryFeatures.logoPath.path}
-              className="text-center w-[100%] object-top"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-r-[2rem]"></div>
-    </div>
-  );
-
   // Radmin (Business Admin)
   const projectFive = (
-    <div className="relative lg:row-span-2">
-      <div className="absolute inset-px rounded-lg bg-white lg:rounded-r-[2rem]"></div>
-      <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-r-[calc(2rem+1px)]">
+    <div className="relative w-[26rem]">
+      <div className="absolute inset-px rounded-lg bg-white"></div>
+      <div className="relative flex h-full flex-col overflow-hidden rounded-lg">
         <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
           <p className="mt-2 text-xl font-medium tracking-tight text-gray-950 max-lg:text-center">
             {radminFeatures.title}
@@ -327,15 +204,15 @@ export default function Page() {
           </div>
         </div>
       </div>
-      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-r-[2rem]"></div>
+      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
     </div>
   );
 
   // Ascend
   const projectSix = (
-    <div className="relative lg:row-span-2">
-      <div className="absolute inset-px rounded-lg bg-white lg:rounded-r-[2rem]"></div>
-      <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-r-[calc(2rem+1px)]">
+    <div className="relative w-[26rem]">
+      <div className="absolute inset-px rounded-lg bg-white"></div>
+      <div className="relative flex h-full flex-col overflow-hidden rounded-lg">
         <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
           <p className="mt-2 text-xl font-medium tracking-tight text-gray-950 max-lg:text-center">
             {ascendFeatures.title}
@@ -392,15 +269,15 @@ export default function Page() {
           </div>
         </div>
       </div>
-      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-r-[2rem]"></div>
+      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
     </div>
   );
 
   // Western Rockies Construction
   const projectSeven = (
-    <div className="relative lg:row-span-2">
-      <div className="absolute inset-px rounded-lg bg-white lg:rounded-r-[2rem]"></div>
-      <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-r-[calc(2rem+1px)]">
+    <div className="relative w-[26rem]">
+      <div className="absolute inset-px rounded-lg bg-white"></div>
+      <div className="relative flex h-full flex-col overflow-hidden rounded-lg">
         <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
           <p className="mt-2 text-xl font-medium tracking-tight text-gray-950 max-lg:text-center">
             {westernRockiesConstruction.title}
@@ -444,7 +321,7 @@ export default function Page() {
           </div>
         </div>
       </div>
-      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-r-[2rem]"></div>
+      <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
     </div>
   );
 
@@ -455,11 +332,9 @@ export default function Page() {
         <p className="mx-auto mt-2 max-w-lg text-balance text-center text-4xl font-semibold tracking-tight text-gray-950 sm:text-5xl">
           Coding for Results.
         </p>
-        <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+        <div className="flex flex-wrap justify-center gap-4">
           {projectOne}
           {projectTwo}
-          {projectThree}
-          {projectFour}
           {projectFive}
           {projectSix}
           {projectSeven}
